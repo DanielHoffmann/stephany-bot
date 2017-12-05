508cef7e5986fbd82c6f22706fab45ffee7a1333
danielhoffmann <danielhoffmann@globalmouth.com>
2017-12-05 14:08:57 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const Bot = require("slackbots");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const TIMEOUT_INTERVAL = 1000;
+// create a bot
+const settings = {
+  token: "xoxb-281244216276-0ixvRD1FnkXn2qoF5xzddGmx",
+  name: "StephanyBot"
+};
+const bot = new Bot(settings);
+bot.on("start", function() {
+  bot.postMessageToChannel(
+    "general",
+    "Hello channel. I am StephanyBot, I automate 95.7% of the work done by Stephanie."
+  );
+});
+
+let timeout = null;
+let lastDaySent = null;
+
+const sendMessage = () => {
+  const today = new Date();
+  if (today.getDate() === lastDaySent) {
+    // already sent message today
+    return;
+  }
+  if (today.getDay() === 5 && today.getHours() === 15) {
+    bot.postMessageToChannel("general", "Do your time reporting! Today!");
+    lastDaySent = today.getDate();
+  }
+  timeout = setTimeout(sendMessage, TIMEOUT_INTERVAL);
+};
+timeout = setTimeout(sendMessage, TIMEOUT_INTERVAL);
